9b45254ba9b8ac9594530d4affad4bb144950ade
Klemen Kenda <klemen.kenda@ijs.si>
2019-11-25 14:43:54 +0100

    Adding bootstrap

--- src/frontend/src/components/App.jsx
+++ src/frontend/src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { 
-    BrowserRouter as Router, 
+import {
+    BrowserRouter as Router,
     Switch,
     Route
 } from "react-router-dom";
 
 import ReactRouterPropTypes from "react-router-prop-types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// bootstrap components
+import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 // components
 import Home from "./Home";
@@ -23,18 +27,53 @@ type State = { };
 
 class App extends React.Component<Props, State> {
     render() {
-        return (
+        return [
             <Router>
                 <Switch>
                     <Route path="/" exact={true}>
-                        <Home />
-                    </Route>                
+                        <Navbar bg="dark" variant="dark">
+                            <Navbar.Brand href="#home">o-gpsy</Navbar.Brand>
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#home">Domov</Nav.Link>
+                                <Nav.Link href="#options">Opcije</Nav.Link>
+                                <Nav.Link href="#pricing">Najem</Nav.Link>
+                            </Nav>
+                            <Form inline>
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                <Button variant="outline-light" className="mr-sm-2">Search</Button>
+                                <Button variant="outline-info">Log In</Button>
+                            </Form>
+                        </Navbar>
+                    </Route>
+                </Switch>
+            </Router>,
+            <Router>
+                <Switch>
+                    <Route path="/" exact={true}>
+                        <Container>
+                            <Home />
+                        </Container>
+                    </Route>
                     <Route path="/live/:id">
                         <Live />
                     </Route>
                 </Switch>
+            </Router>,
+            <Router>
+                <Switch>
+                    <Route path="/" exact={true}>
+                        <footer className="text-muted mt-5">
+                            <div className="container">
+                                <p className="float-right">
+                                    <a href="#home">Back to top</a>
+                                </p>
+                                <p>&copy;Klemen Kenda</p>
+                            </div>
+                        </footer>
+                    </Route>
+                </Switch>
             </Router>
-        );
+        ];
     }
 }
 
