7a1e25e7705ab4b5e1a395a1b70b08478273372f
Klemen Kenda <klemen.kenda@ijs.si>
2019-11-25 19:00:50 +0100

    Requests for HTTP locations

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,21 @@
 // imports
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
 
 const gps = require('./controller/gps');
 
-app.get('/api/register/:u/:p/:x/:y/', (req, res) => {
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.get('/api/register/:u/:p/:x/:y/:t', (req, res) => {
     console.log(req.params);
+    res.send("OK");
 });
+
+
+
 
 app.listen(3000);
