2b40af06380173064973474d0c3f37afd42c4c96
Klemen Kenda <klemen.kenda@ijs.si>
2019-12-03 09:13:37 +0100

    Cleanup

--- src/frontend/src/components/Login.jsx
+++ src/frontend/src/components/Login.jsx
@@ -50,31 +50,6 @@ class Login extends Component<Props, State> {
         </Row>
     }
 
-    // obsolete
-    render2() {
-        return <Container>
-            <Row className="justify-content-md-center">
-                <Col xs={12} md={6} lg={6}>
-                    <Form className="form-signin">
-                        <img className="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
-                        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
-                        <label htmlFor="inputPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                        <div className="checkbox mb-3">
-                            <label>
-                            <input type="checkbox" value="remember-me" /> Remember me
-                            </label>
-                        </div>
-                        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                        <p className="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    }
-
 }
 
 export default Login;
